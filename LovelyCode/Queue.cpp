5258300065a4b431d5dbaff68351fc7905d5d757
Lovely <92400963+LovelyBika@users.noreply.github.com>
2023-11-08 15:24:23 +0530

    Queue Template

---
+++ LovelyCode/Queue.cpp
@@ -0,0 +1,36 @@
+class Queue {
+
+	int front, rear;
+	vector<int> arr;
+
+public:
+	Queue()
+	{
+		front = 0;
+		rear = 0;
+		arr.resize(100001);
+	}
+
+	// Enqueue (add) element 'e' at the end of the queue.
+	void enqueue(int e)
+	{
+		// Write your code here.
+		  arr[rear]=e;
+		  rear++;
+	}
+
+	// Dequeue (retrieve) the element from the front of the queue.
+	int dequeue()
+	{
+		// Write your code here.
+		 if(rear==front)
+		   return -1;
+		 else {
+			 int val = arr[front];
+			  front++;
+			  return val;
+
+		 }  
+
+	}
+};
